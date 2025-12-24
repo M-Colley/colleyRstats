89c7a3619306d3e77f362bd3d4161bd7b84c5452
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2025-12-24 14:24:42 +0100

    Update helper-load.R

--- tests/testthat/helper-load.R
+++ tests/testthat/helper-load.R
@@ -1,4 +1,5 @@
-r_files <- list.files("R", pattern = "\\.R$", full.names = TRUE)
+package_root <- normalizePath(file.path("..", ".."), mustWork = TRUE)
+r_files <- list.files(file.path(package_root, "R"), pattern = "\\.R$", full.names = TRUE)
 for (file in r_files) {
   sys.source(file, envir = globalenv())
 }

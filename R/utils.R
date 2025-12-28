f435475b64e9fef15d83e90f820cc0f215cc205f
Mark Colley <39366189+M-Colley@users.noreply.github.com>
2025-12-28 10:49:07 +0100

    update

--- R/utils.R
+++ R/utils.R
@@ -23,11 +23,13 @@ not_empty <- function(x, msg = "Input must not be empty.") {
 #' @param x Vector of values to test.
 #' @param y Vector of values to match against.
 #' @return Logical vector indicating non-membership.
-#' @name not_in
-#' @aliases %!in%
-#' @usage x %!in% y
 #' @export
-"%!in%" <- function(x, y) !("%in%"(x, y))
+not_in <- function(x, y) !(x %in% y)
+
+#' @rdname not_in
+#' @export
+`%!in%` <- not_in
+
 
 #' Replace NA values with zero
 #'
